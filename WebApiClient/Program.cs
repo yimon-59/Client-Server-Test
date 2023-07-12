cfe59d01a017d0dcf552e2161310b1522e9c3b98
yimon <emikospring5@gmail.com>
2023-07-12 15:37:18 +0630

    Add project files.

---
+++ WebApiClient/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace WebApiClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("press any key cont.....");
+            Console.ReadLine();
+
+            using(HttpClient httpClient = new HttpClient())
+            {
+                var response = await httpClient.GetAsync("http://localhost:5038/values");
+                response.EnsureSuccessStatusCode();
+                if(response.IsSuccessStatusCode)
+                {
+                    string message = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(message);
+                }
+                else
+                {
+                    Console.WriteLine($"response error code : {response.StatusCode}");
+                }
+            }
+            Console.ReadLine();
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ WebApiServer/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiServer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public string[] Get()
+        {
+            return new string[] { "mango", "strawberry", "blueberry", "pear" };
+        }
+    }
+}
